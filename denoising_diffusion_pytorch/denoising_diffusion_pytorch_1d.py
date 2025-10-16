e8cd7ea9b8e860d2eee9871a5fdc03760a2b9abf
Amirhossein Ramazani <ramazani.amirhossein@gmail.com>
2025-10-16 11:49:44 +0330

    fix bugs

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch_1d.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch_1d.py
@@ -92,14 +92,14 @@ class Residual(Module):
     def forward(self, x, *args, **kwargs):
         return self.fn(x, *args, **kwargs) + x
 
-def Upsample(dim, dim_out = None):
-    return nn.Sequential(
-        nn.Upsample(scale_factor = 2, mode = 'nearest'),
-        nn.Conv1d(dim, default(dim_out, dim), 3, padding = 1)
-    )
+    def Upsample(dim, dim_out = None):
+        return nn.Sequential(
+            nn.Upsample(scale_factor = 2, mode = 'nearest'),
+            nn.Conv1d(dim, default(dim_out, dim), 3, padding = 1)
+        )
 
-def Downsample(dim, dim_out = None):
-    return nn.Conv1d(dim, default(dim_out, dim), 4, 2, 1)
+    def Downsample(dim, dim_out = None):
+        return nn.Conv1d(dim, default(dim_out, dim), 4, 2, 1)
 
 class RMSNorm(Module):
     def __init__(self, dim):
